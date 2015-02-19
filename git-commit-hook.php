e2bf83ad0751966f7edc55a56e76395f18a530da
Brandon Wood <bcwood@gmail.com>
2015-02-19 07:29:06 -0900

    verify secret in commit hook

--- git-commit-hook.php
+++ git-commit-hook.php
@@ -1,12 +1,36 @@
 <?php
 
-require_once("config.php");
+if ($_SERVER["REQUEST_METHOD"] != "POST" || strpos($_SERVER["HTTP_USER_AGENT"], "GitHub-Hookshot/") === FALSE)
+    die("Bad request");
 
-echo "cd {${LOCAL_PATH}} && git pull <br>";
+require_once("git-config.php");
 
-exec("cd {${LOCAL_PATH}} && git pull")
+// verify X-Hub-Signature
+$headers = getallheaders();
+
+if (empty($headers['X-Hub-Signature']))
+    die("No signature");
+
+$signature = $headers['X-Hub-Signature'];
+ 
+// split signature into algorithm and hash
+list($algorithm, $hash) = explode('=', $signature, 2);
+ 
+// calculate hash based on payload and the secret
+$payload = file_get_contents('php://input');
+$payloadHash = hash_hmac($algorithm, $payload, COMMIT_HOOK_SECRET);
+ 
+// compare hashes
+if ($hash !== $payloadHash)
+    die("Bad signature");
+
+$local_path = getcwd();
+
+echo "cd {$local_path} && git pull <br>";
+
+exec("cd {$local_path} && git pull")
     or die("git pull failed!");
 
-echo "done updating!";
+echo "Update completed successfully!";
 
 ?>
