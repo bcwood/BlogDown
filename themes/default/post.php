2acc0b53fc2bd8ab5b88d07e35e3f6d9fdcac5f7
Brandon Wood <bcwood@gmail.com>
2015-02-23 14:02:52 -0900

    fixed post css classes

--- themes/default/post.php
+++ themes/default/post.php
@@ -1,8 +1,8 @@
 <h1><?php echo $post->title; ?></h1>
 
-<div class="meta">
+<div class="post-meta">
     Posted <?php echo $post->date->format("Y-m-d"); ?>
 </div>
-<div class="page">
+<div class="post">
     <?php echo $post->body; ?>
 </div>
